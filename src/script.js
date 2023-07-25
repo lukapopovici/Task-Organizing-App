60da2b1ca428cf040cd260b0bbf127d3217913a7
Luka Popovici <lukapopovici@users.noreply.github.com>
2023-07-25 15:18:34 +0300

    commit

--- src/script.js
+++ src/script.js
@@ -14,16 +14,32 @@ function handleSubmit(event) {
     dialog.style.display = 'none';
     add_task(TI,TD,TE);
 }
+
+function taskDone(button) {
+  const task = button.parentNode;
+  const isDone = task.getAttribute('isDone') === 'true';
+
+  if (!isDone) {
+    task.setAttribute('isDone', 'true');
+    task.style.backgroundColor = 'green';
+    button.parentNode.removeChild(button);
+  } 
+}
+
 function add_task(taskTitle, taskDesc, taskEmail) {
-    const task = document.createElement('div');
-    task.id = "TaskID";
-    task.innerHTML = `
-      <strong>Title:</strong> ${taskTitle}<br>
-      <strong>Description:</strong> ${taskDesc}<br>
-      <strong>Email:</strong> ${taskEmail}
-    `;
-    document.getElementById('tasks').appendChild(task);
-  }
-  
+
+  const task = document.createElement('div');
+  task.id = "TaskID";
+  task.setAttribute('isDone', false);
+  task.innerHTML = `
+    <strong>Title:</strong> ${taskTitle}<br>
+    <strong>Description:</strong> ${taskDesc}<br>
+    <strong>Email:</strong> ${taskEmail}<br>
+    <button onclick="this.parentNode.remove()">Remove</button><br>
+    <button onclick="taskDone(this)">Completed</button><br>
+  `;
+  document.getElementById('tasks').appendChild(task);
+
+}
 
   document.getElementById('FORM').addEventListener('submit', handleSubmit);
